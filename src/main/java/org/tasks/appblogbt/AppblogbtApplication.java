96c768af01ebfd95a614ddad896c76c5a3178e4e
Alex Turaev <a.turaev@r77.center-inform.ru>
2025-03-19 13:30:32 +0300

    first commit

---
+++ src/main/java/org/tasks/appblogbt/AppblogbtApplication.java
@@ -0,0 +1,13 @@
+package org.tasks.appblogbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppblogbtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppblogbtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tasks/appblogbt/AppblogbtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tasks.appblogbt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppblogbtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
